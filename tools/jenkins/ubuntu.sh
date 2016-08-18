d58df993dc0f9710357d4142c585b50e4affdcda
sunhuang2005 <sunhuang2005@gmail.com>
2016-08-18 11:22:14 +0800

    jenkins sources.list

--- tools/jenkins/ubuntu.sh
+++ tools/jenkins/ubuntu.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 wget -q -O - http://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo "deb http://pkg.jenkins.io/debian-stable binary/" > /etc/apt/sources.list'
+sudo sh -c 'echo "deb http://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
